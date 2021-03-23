0b209c83002127047cac08d83574094905803977
Smith <smith.mugalu@andela.com>
2021-03-23 16:18:02 +0300

    initial commit

---
+++ Raintaculous/Raintaculous/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Raintaculous
+//
+//  Created by Smithers on 23/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
